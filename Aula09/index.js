19d0eebdec4c7f03c69da19722ee8a30d521da44
Natanael Vieira <natanaelsales@live.com>
2023-02-20 11:30:08 -0300

    Create index.js

---
+++ Aula09/index.js
@@ -0,0 +1,18 @@
+function mensagemNatan(callback) {
+    setTimeout(() => {
+        console.log('Testanto a função de time out')
+        callback()
+    }, 0);
+}
+
+function mensagemNatanael() {
+    console.log('Então, assim esta será a primeira mensagem!')
+}
+
+mensagemNatan(mensagemNatanael)
+//mensagemNatanael()
+
+//Mas para garantir que a mensagem que tenha o setTimeout ainda assim reproduza primeiro, basta criar um parâmetro e utilizar a função que viria a seguir, 
+//mas reproduziria primeiro, chamando a função como parâmetro. Garantindo assim o sincronismo das chamadas, mas precisa ficar esperto para não criar uma
+//complexidade desnecessária.
+
