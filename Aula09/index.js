d04330f307741dafc89f2d67d4bd215276015130
Natanael Vieira <natanaelsales@live.com>
2023-02-21 05:04:04 -0300

    Update index.js

--- Aula09/index.js
+++ Aula09/index.js
@@ -1,18 +1,20 @@
-function mensagemNatan(callback) {
-    setTimeout(() => {
-        console.log('Testanto a função de time out')
-        callback()
-    }, 0);
+function mensagemNatan() {
+    const promiseMensagemNatan = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Testanto a função de time out');
+            resolve();
+        }, 0);
+    });
+    return promiseMensagemNatan;
 }
 
 function mensagemNatanael() {
-    console.log('Então, assim esta será a primeira mensagem!')
+    console.log('Então, assim esta será a primeira mensagem!');
 }
 
-mensagemNatan(mensagemNatanael)
-//mensagemNatanael()
+mensagemNatan().then();
+mensagemNatanael();
 
-//Mas para garantir que a mensagem que tenha o setTimeout ainda assim reproduza primeiro, basta criar um parâmetro e utilizar a função que viria a seguir, 
-//mas reproduziria primeiro, chamando a função como parâmetro. Garantindo assim o sincronismo das chamadas, mas precisa ficar esperto para não criar uma
-//complexidade desnecessária.
-
+/*Refatorando o callback utilizamos a promise para fazer com que esta sequência de mensagens seja exibidas na ordem que eu 
+chamei elas, ou seja, na sequência que coloquei cada uma delas no código, e sem o callback, utilizando a promise, 
+ele vai garantir com que esta sequência seja obedecida.*/
