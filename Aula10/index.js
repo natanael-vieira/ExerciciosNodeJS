76804cd922f8175aa029938813a12fc69b30c0df
Natanael Vieira <natanaelsales@live.com>
2023-02-21 06:49:30 -0300

    Update index.js

--- Aula10/index.js
+++ Aula10/index.js
@@ -1,9 +1,22 @@
 import fetch from 'node-fetch';
 
-function main() {
-    console.log('main(');
-    const user = 'natan-vieira'
-    fetch(``)
+// function main() {
+//     console.log('main(');
+//     const user = 'natanael-vieira';
+//     const apiUrl = 'https://api.github.com';
+//     fetch(`${apiUrl}/users/${user}`).then((response) => {
+//         return response.json();
+//     }).then((user) => {
+//         console.log(user);
+//     })
+// }
+
+async function main() {
+    const user = 'natanael-vieira';
+    const apiUrl = 'https://api.github.com';
+    const response = await fetch (`${apiUrl}/users/${user}`)
+    const githubUser = await response.json();
+    console.log(githubUser.name)
 }
 
 main();
