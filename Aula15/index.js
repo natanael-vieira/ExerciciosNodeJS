0a2f5980d66702b97f8b47f61ba23f87a8c39181
Natanael Vieira <natanaelsales@live.com>
2023-02-22 06:37:46 -0300

    Create index.js

---
+++ Aula15/index.js
@@ -0,0 +1,15 @@
+import { getPeople } from "./starWarsService.js";
+
+async function main() {
+    const starWarsPeople = await getPeople(1);
+    const starWarsPeopleFiltered = starWarsPeople
+    .filter(person => {
+        return person.gender === 'female'})
+    .map(person => {
+        return {name: person.name, gender: person.gender}
+    });
+
+    console.log(starWarsPeopleFiltered)
+}
+
+main();
