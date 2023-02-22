4d55e97854011173b38aaa74bd531f026d79999b
Natanael Vieira <natanaelsales@live.com>
2023-02-22 05:32:21 -0300

    Update index.js

--- Aula13/index.js
+++ Aula13/index.js
@@ -3,8 +3,10 @@ import { getPeople } from "./starWarsService.js";
 async function main() {
     const starWarsPeople = await getPeople(1);
     const starWarsPeopleName = starWarsPeople.map((person) => {
-        const name = person.name;
-        const height = person.height;
+        // const name = person.name;
+        // const height = person.height; 
+        const {name, height} = person; /* Neste caso eu substituí as duas constantes por uma só utilizando chaves, como se fosse um objeto, dessa forma eu não altero 
+        o código em nada, apenas apresento ele em uma linha diminuindo meu código, assim como deixar ele mais elegante */
         return {
             name, height
         }
