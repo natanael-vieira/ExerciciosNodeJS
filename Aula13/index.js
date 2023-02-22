224492a00bbf919565a544ff9d782562294b3ad3
Natanael Vieira <natanaelsales@live.com>
2023-02-22 05:20:49 -0300

    Update index.js

--- Aula13/index.js
+++ Aula13/index.js
@@ -1,16 +1,15 @@
 import { getPeople } from "./starWarsService.js";
 
 async function main() {
-    const starWarsPeople = await getPeople(2);
-    // for(let i = 0; i < starWarsPeople.length; i++) {
-    //     console.log(starWarsPeople[i].name);
-    // }
-    // for(let propPerson in starWarsPeople[0]) {
-    //     console.log(starWarsPeople[0][propPerson]);
-    // }
-    for(let person of starWarsPeople) {
-        console.log(person.name);
-    }
+    const starWarsPeople = await getPeople(1);
+    const starWarsPeopleName = starWarsPeople.map((person) => {
+        const name = person.name;
+        const height = person.height;
+        return {
+            name, height
+        }
+    });
+    console.log(starWarsPeopleName);
 }
 
 main();
