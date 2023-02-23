21fe6d7a8276285249c2cfdfb5f198caeab32f97
Natanael Vieira <natanaelsales@live.com>
2023-02-23 19:11:46 -0300

    Update index.js

--- Aula18-GET/index.js
+++ Aula18-GET/index.js
@@ -2,11 +2,27 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
+let users = [
+    {id: 1, name: 'Natan Vieira', age: 35},
+    {id: 2, name: 'Thais Vieira', age: 35},
+]
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 app.get('/', (request, response) => {
-    return response.send('<h1>Trabalhando com servidor express.</h1>')
+    return response.send('<h1>Trabalhando com servidor express.</h1>');
+});
+
+app.get('/users', (request, response) => {
+    return response.send(users);
+})
+
+app.get('/users/:userId', (request, response) => {
+    const userId = request.params.userId;
+    const user = users.find(user => {
+        return (user.id === Number(userId))
+    })
+    return response.send(user);
 });
