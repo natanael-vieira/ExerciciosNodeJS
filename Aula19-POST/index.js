23c456cf10f44543382e8a427f6a9a7e02a95d6e
Natanael Vieira <natanaelsales@live.com>
2023-02-24 04:46:22 -0300

    Update index.js

---
+++ Aula19-POST/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+let users = [
+    {id: 1, name: 'Natan Vieira', age: 35},
+    {id: 2, name: 'Thais Vieira', age: 35},
+    {id: 3, name: 'Abigail Vieira', age: 4},
+    {id: 4, name: 'Rute Vieira', age: 0},
+]
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get('/', (request, response) => {
+    return response.send('<h1>Trabalhando com servidor express.</h1>');
+});
+
+app.get('/users', (request, response) => {
+    return response.send(users);
+})
+
+app.get('/users/:userId', (request, response) => {
+    const userId = request.params.userId;
+    const user = users.find(user => {
+        return (user.id === Number(userId))
+    })
+    return response.send(user);
+});
