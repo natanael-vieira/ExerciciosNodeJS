dfa48741549c0aa46229459513a8828c244446a6
Natanael Vieira <natanaelsales@live.com>
2023-02-27 06:13:21 -0300

    Update index.js

--- Aula23-EJS/index.js
+++ Aula23-EJS/index.js
@@ -4,10 +4,13 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 app.get('/', (request, response) => {
-    response.send('<h1>Trabalhando com EJS</h1>')
+    response.render('pages/home', {title: 'EJS'});
 });
