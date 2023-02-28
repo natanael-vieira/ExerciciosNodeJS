4d14d4c8ac913fc9962aec3b95e693732ca4706b
Natanael Vieira <natanaelsales@live.com>
2023-02-28 05:35:17 -0300

    Update ArticleService.js

--- Aula23-EJS/ArticleService.js
+++ Aula23-EJS/ArticleService.js
@@ -1,12 +1,12 @@
 const articles = [
     {
         id: 1,
-        title: 'Healttech de "telemedicina aumentada" chega ao Brasil',
+        title: 'Healthtech de "telemedicina aumentada" chega ao Brasil',
         description: 'Loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum.'
     },
     {
         id: 2,
-        title: 'Testa: entregas caem com lockdown na China',
+        title: 'Tesla: entregas caem com lockdown na China',
         description: 'Loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum, loren ipsum.'
     }
 ];
@@ -17,7 +17,7 @@ function getArticle() {
 
 function getArticleById(articleId) {
     return articles.find(article => {
-        article.id === Number(articleId)
+        return article.id === Number(articleId)
     });
 }
 
